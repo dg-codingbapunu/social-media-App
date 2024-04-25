85e9240af70a23b6ec9c428c2fdffbe35f6ee28f
patitapaban <patitapabanpanda046@gmail.com>
2024-04-25 12:08:02 +0530

    add postlist

--- social/src/App.jsx
+++ social/src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
 import CreatePost from "./components/Createpost";
-import Posts from "./components/Posts";
+import PostList from "./components/PostList";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <div className="content">
         <Header></Header>
         <CreatePost></CreatePost>
-        <Posts></Posts>
+        <PostList></PostList>
         <Footer></Footer>
       </div>
     </div>
