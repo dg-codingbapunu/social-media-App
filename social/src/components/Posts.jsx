4ee3307b1833a10e994694b52f179453644b81e8
patitapaban <patitapabanpanda046@gmail.com>
2024-04-26 13:21:00 +0530

    add react icon library

--- social/src/components/Posts.jsx
+++ social/src/components/Posts.jsx
@@ -1,17 +1,20 @@
+import { MdDelete } from "react-icons/md";
+
 const Posts = ({ posts }) => {
   return (
-    <div className="card post-card" style={{ width: "18rem" }}>
+    <div className="card post-card">
       <div className="card-body">
         <h5 className="card-title">
           {posts.title}
           <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-            {posts.reactions}
+            <MdDelete />
           </span>
         </h5>
         <p className="card-text">{posts.body}</p>
         {posts.tags.map((tag) => (
-          <span class="badge text-bg-primary hasthag">{tag}</span>
+          <span className="badge text-bg-primary hasthag">{tag}</span>
         ))}
+        <span className="badge text-bg-success ">{posts.reactions}</span>
       </div>
     </div>
   );
